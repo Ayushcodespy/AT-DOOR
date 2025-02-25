5ee67f2af4e8c6d84cfcd0db0825037cf0b48f01
Ayush <ayush.jnv54@gmail.com>
2025-02-25 15:13:25 +0530

    added some new widgets

--- atdoor/lib/common/widgets/custom_shapes/containers/search_container.dart
+++ atdoor/lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -12,13 +12,15 @@ class TSearchContainer extends StatelessWidget {
     this.icon = Iconsax.search_normal,
     this.showBackground = true,
     this.showBorder = true,
-    this.onTap
+    this.onTap,
+    this.padding = const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace)
   });
 
   final String text;
   final IconData? icon;
   final bool showBackground, showBorder;
   final VoidCallback? onTap;
+  final EdgeInsetsGeometry padding;
 
   @override
   Widget build(BuildContext context) {
@@ -27,10 +29,10 @@ class TSearchContainer extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+        padding: padding,
         child: Container(
           width: TDeviceUtils.getScreenWidth(context),
-          padding: const EdgeInsets.all(TSizes.md),
+          padding: const EdgeInsets.all(TSizes.md*0.6),
           decoration: BoxDecoration(
               color: showBackground
                   ? dark
