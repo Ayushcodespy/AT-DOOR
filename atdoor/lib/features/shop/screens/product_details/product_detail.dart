8dc1dca7fd269f5415485b888e785d1260cefa83
Ayush <ayush.jnv54@gmail.com>
2025-03-15 21:21:08 +0530

    updated

--- atdoor/lib/features/shop/screens/product_details/product_detail.dart
+++ atdoor/lib/features/shop/screens/product_details/product_detail.dart
@@ -3,8 +3,11 @@ import 'package:atdoor/features/shop/screens/product_details/widgets/product_att
 import 'package:atdoor/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
 import 'package:atdoor/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:atdoor/features/shop/screens/product_details/widgets/rating_share_widget.dart';
+import 'package:atdoor/features/shop/screens/product_reviews/product_reviews.dart';
 import 'package:atdoor/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:readmore/readmore.dart';
 
@@ -65,7 +68,7 @@ class ProductDetailScreen extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       const TSectionHeading(title: 'Reviews(199)', showActionButton: false,),
-                      IconButton(icon: const Icon(Iconsax.arrow_right_3, size: 18), onPressed: (){})
+                      IconButton(icon: const Icon(Iconsax.arrow_right_3, size: 18), onPressed: () => Get.to(() => const ProductReviewsScreen()))
                     ],
                   ),
 
