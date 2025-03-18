1867a0087a4d95374aa0cc051b5c2a1b64400bb9
Ayush <ayush.jnv54@gmail.com>
2025-03-18 22:57:25 +0530

    updated

--- atdoor/lib/utils/constants/image_strings.dart
+++ atdoor/lib/utils/constants/image_strings.dart
@@ -36,8 +36,18 @@ class TImages {
     static const String promoBanner3 = "assets/images/banners/promo-3.jpg";
 
 
-    // ---- Products
+    // ---- Profile
     static const String user = "assets/images/user/user.png";
+
+    // ---- Payment Options
+    static const String paypal = "assets/icons/payment_methods/paypal.png";
+    static const String googlePay = "assets/icons/payment_methods/google-pay.png";
+    static const String creditCard = "assets/icons/payment_methods/credit-card.png";
+    static const String masterCard = "assets/icons/payment_methods/mastercard.png";
+    static const String visa = "assets/icons/payment_methods/visa.png";
+    static const String paytm = "assets/icons/payment_methods/paytm.png";
+    static const String phonePe = "assets/icons/payment_methods/phonepe.png";
+    static const String successfulPaymentIcon = "assets/icons/payment_methods/successful.png";
 
 
     // ---- Products
